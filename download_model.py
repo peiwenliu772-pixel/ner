a0339eb56773df839b69f1cf96e6f5a83ef54c47
JohnDoe <johndoe@example.com>
2025-11-19 19:17:05 +0800

    将预训练权重文件下载到本地

--- download_model.py
+++ download_model.py
@@ -1,8 +1,18 @@
-
 from huggingface_hub import snapshot_download
 
 model_path = snapshot_download(
     repo_id="hfl/chinese-bert-wwm",
-    cache_dir="./pre_models/bert-wwm" 
+    local_dir="./pre_models/chinese-bert-wwm",
+    local_dir_use_symlinks=False,
+    allow_patterns=[
+        "pytorch_model.bin",   # PyTorch 权重
+        "config.json",         # 模型结构
+        "vocab.txt",           # tokenizer 词表
+        "tokenizer.json"       # tokenizer 配置
+    ],
+    resume_download=True
+
 )
-print(f"模型已下载到:{model_path}")
+
+print(f"模型已下载到: {model_path}")
+# google-bert/bert-base-chinese
